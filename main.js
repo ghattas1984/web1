0607ede350aa9d3a90eac9ab6bacf42f6f36a74a
ghattas1984 <gattas.it.1984@gmail.com>
2024-12-02 08:28:21 +0200

    upload files 02122024

---
+++ main.js
@@ -0,0 +1,15 @@
+const img = document.getElementById("main-img");
+
+function phone(x){
+    img.src = x;
+}
+
+let contant = document.querySelector(".contanter");
+
+
+
+function color(co){
+
+    contant.style.backgroundColor  = co;
+
+}
